5c7af455ed2a75831059a2153fba53a32c3812c0
frncabre <127581455+frncabre@users.noreply.github.com>
2023-11-17 15:28:20 -0300

    Add files via upload
    Version casi final

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #Ejecutar con python en vez de python3
-import tkinter as tk 
 from PIL import Image, ImageTk
+import tkinter as tk
+import tkinter.simpledialog
+import random
+import sqlite3
 
-ventana = tk.Tk()
-ventana.geometry('1000x600')
-ventana.title('Flappy Bird')
+
+window = tk.Tk()
+window.geometry('1000x600')
+window.title('Flappy Bird')
 
 x = 150
 y = 300
@@ -15,7 +19,7 @@ fin_juego = False
 img_pajaro = Image.open('img/bird.png')
 img_pajaro = ImageTk.PhotoImage(img_pajaro)
 
-img_tubo_arriba = Image.open('img/pipe.png')        
+img_tubo_arriba = Image.open('img/pipe.png') 
 img_tubo_abajo = img_tubo_arriba.rotate(180)
 
 img_tubo_arriba = ImageTk.PhotoImage(img_tubo_arriba)
@@ -24,35 +28,65 @@ img_tubo_abajo = ImageTk.PhotoImage(img_tubo_abajo)
 img_reiniciar = Image.open('img/restart.png')
 img_reiniciar = ImageTk.PhotoImage(img_reiniciar)
 
-canvas = tk.Canvas(ventana, highlightthickness=0, bg= '#00bfff')
+canvas = tk.Canvas(window, highlightthickness=0, bg='#00bfff')
 canvas.place(relwidth=1, relheight=1)
 
 texto_puntuacion = canvas.create_text(50, 50, text='0', fill='white', font=('D3 Egoistism outline', 30))
 
 pajaro = canvas.create_image(x, y, anchor='nw', image=img_pajaro)
-tubo_arriba = canvas.create_image(1200, -550, anchor='nw', image=img_tubo_arriba)
-tubo_abajo = canvas.create_image(1200, 550, anchor='nw', image=img_tubo_abajo)
+tubo_arriba = canvas.create_image(1200, -550, anchor='nw', image=img_tubo_abajo)
+tubo_abajo = canvas.create_image(1200, 550, anchor='nw', image=img_tubo_arriba)
 
-def mover_pajaro_tecla(evento):
+def conexionBD(consulta, parametros=()):
+    with sqlite3.connect("Flappy-Bird.db") as conexion:
+        cursor = conexion.cursor()
+        resultado_con = cursor.execute(consulta, parametros)
+        conexion.commit()
+        return resultado_con
+
+def mover_pajaro_tecla(event):
     global x, y
     if not fin_juego:
         y -= 30
         canvas.coords(pajaro, x, y)
 
-ventana.bind("<space>", mover_pajaro_tecla)
 
-def fin_del_juego():
-    global fin_juego
-    fin_juego = True
+window.bind("<space>", mover_pajaro_tecla)
+
 
 def mover_pajaro():
     global x, y
     y += 5
     canvas.coords(pajaro, x, y)
-    if y < 0 or y > ventana.winfo_height():
+    if y < 0 or y > window.winfo_height():
         fin_del_juego()
+
     if not fin_juego:
-        ventana.after(50, mover_pajaro)
+        window.after(50, mover_pajaro)
+
+
+def mover_tubo():
+    global puntuacion, fin_juego, velocidad
+    canvas.move(tubo_arriba, -velocidad, 0)
+    canvas.move(tubo_abajo, -velocidad, 0)
+    if canvas.coords(tubo_abajo)[0] < -100:
+        puntuacion += 1
+        velocidad += 1
+        canvas.itemconfigure(texto_puntuacion, text=str(puntuacion))
+        h = window.winfo_height()
+        num = random.choice([i for i in range(160, h, 160)])
+        canvas.coords(tubo_abajo, window.winfo_width(), num + 160)
+        canvas.coords(tubo_arriba, window.winfo_width(), num - 900)
+
+    if 0 < canvas.coords(tubo_abajo)[0] < 160:
+        if canvas.bbox(pajaro)[0] < canvas.bbox(tubo_abajo)[2] and canvas.bbox(pajaro)[2] > canvas.bbox(
+                tubo_abajo)[0]:
+            if canvas.bbox(pajaro)[1] < canvas.bbox(tubo_arriba)[3] or canvas.bbox(pajaro)[3] > canvas.bbox(
+                    tubo_abajo)[1]:
+                fin_del_juego()
+    if not fin_juego:
+        window.after(50, mover_tubo)
+
 
 def reiniciar_juego():
     global x, y, puntuacion, velocidad, fin_juego
@@ -65,12 +99,54 @@ def reiniciar_juego():
     canvas.coords(tubo_arriba, 1200, -550)
     canvas.coords(tubo_abajo, 1200, 550)
     canvas.itemconfigure(texto_puntuacion, text="0")
-    etiqueta_fin_juego.place_forget()
-    boton_reiniciar.place_forget()
+    lbl_fin_juego.place_forget()
+    bt_reiniciar.place_forget()
+    mover_pajaro()
+    mover_tubo()
 
-etiqueta_fin_juego = tk.Label(ventana, text="Perdiste", font=('D3 Egoistism outline', 30), fg="white", bg="#00bfff")
-boton_reiniciar = tk.Button(ventana, border=0, image=img_reiniciar, activebackground='#00bfff', bg='#00bfff', command=reiniciar_juego)
+def ingresar_nombre_puntaje():
+    nombre_jugador = tk.simpledialog.askstring("Nombre", "Ingrese su nombre (máximo 6 caracteres):")
+    while nombre_jugador is None or len(nombre_jugador) > 6:
+        if nombre_jugador is None:
+            # El usuario cerró el cuadro de diálogo, pedir nuevamente el nombre
+            nombre_jugador = tk.simpledialog.askstring("Nombre", "Ingrese su nombre (máximo 6 caracteres):")
+        else:
+            # El nombre ingresado es demasiado largo, pedir nuevamente el nombre
+            tk.messagebox.showwarning("Advertencia", "El nombre debe tener como máximo 6 caracteres.")
+            nombre_jugador = tk.simpledialog.askstring("Nombre", "Ingrese su nombre (máximo 6 caracteres):")
 
-ventana.after(50, mover_pajaro)
+    return nombre_jugador
 
-ventana.mainloop()
+def fin_del_juego():
+    global fin_juego
+    fin_juego = True
+    lbl_fin_juego.place(relx=0.5, rely=0.5, anchor='center')
+    bt_reiniciar.place(relx=0.5, rely=0.7, anchor='center')
+
+    # Solicitar al jugador un nombre mediante el cuadro de diálogo
+    nombre_jugador = ingresar_nombre_puntaje()
+
+    # Subir el puntaje y el nombre a la base de datos
+    consulta = "INSERT INTO Jugador (nombre, puntaje) VALUES ('{0}', '{1}')".format(nombre_jugador, puntuacion)
+    conexionBD(consulta)
+    
+    while canvas.coords(pajaro):
+        if canvas.bbox(pajaro)[0] < canvas.bbox(tubo_abajo)[2] and canvas.bbox(pajaro)[2] > canvas.bbox(tubo_abajo)[
+            0]:
+            if canvas.bbox(pajaro)[1] < canvas.bbox(tubo_arriba)[3] or canvas.bbox(pajaro)[3] > canvas.bbox(
+                    tubo_abajo)[1]:
+                break
+    lbl_fin_juego.place(relx=0.5, rely=0.5, anchor='center')
+    bt_reiniciar.place(relx=0.5, rely=0.7, anchor='center')
+
+
+lbl_fin_juego = tk.Label(window, text='¡Juego Terminado!', font=('D3 Egoistism outline', 30), fg='white', bg='#00bfff')
+bt_reiniciar = tk.Button(window, border=0, image=img_reiniciar, activebackground='#00bfff', bg='#00bfff',
+                          command=reiniciar_juego)
+
+window.after(50, mover_pajaro)
+window.after(50, mover_tubo)
+
+window.call('wm', 'iconphoto', window._w, img_pajaro)
+window.mainloop()
+
